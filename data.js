1584a9d965c8ad3c06a79dbb334015a5c5e76eb7
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-15 17:09:30 +0300

    here we goooo

---
+++ data.js
@@ -0,0 +1,6 @@
+export default tweets = [
+  {
+    header: 'The New York Times',
+    content: 'Breaking News: The U.S. has surpassed 500,000 deaths from the coronavirus, a once-unimaginable toll.',
+
+  }]
