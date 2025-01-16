1a3344e3c050ef37aac83fdb05eb26674299448c
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-16 21:31:23 +0300

    added images

--- data.js
+++ data.js
@@ -1,6 +1,4 @@
-export default tweets = [
-  {
-    header: 'The New York Times',
-    content: 'Breaking News: The U.S. has surpassed 500,000 deaths from the coronavirus, a once-unimaginable toll.',
-
-  }]
+export const tweetsData = [{
+  handle: "Kaleab Shewangizaw",
+  profilePic: ""
+}]
