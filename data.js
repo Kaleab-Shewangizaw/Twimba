c52533009a5fc3b95d21bbb52bcdadb2071a7962
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-16 21:36:42 +0300

    npm init

--- data.js
+++ data.js
@@ -1,4 +1,88 @@
 export const tweetsData = [{
   handle: "Kaleab Shewangizaw",
-  profilePic: ""
-}]
+  profilePic: "./images/pp1.jpg",
+  likes: 100,
+  retweets: 200,
+  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+  replies: [
+    {
+      handle: "John Doe",
+      profilePic: "./images/pp2.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+    },
+    {
+      handle: "Jane Doe",
+      profilePic: "./images/pp3.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
+    }
+]},
+{
+  handle: "John Doe",
+  profilePic: "./images/pp2.jpg",
+  likes: 100,
+  retweets: 200,
+  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+  replies: [
+    {
+      handle: "Kaleab Shewangizaw",
+      profilePic: "./images/pp1.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+    },
+    {
+      handle: "Jane Doe",
+      profilePic: "./images/pp3.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
+    }
+]},
+{
+  handle: "Jane Doe",
+  profilePic: "./images/pp3.jpg",
+  likes: 100,
+  retweets: 200,
+  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+  replies: [
+    {
+      handle: "Kaleab Shewangizaw",
+      profilePic: "./images/pp1.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+    },
+    {
+      handle: "John Doe",
+      profilePic: "./images/pp2.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
+    }
+]},
+{
+  handle: "Kaleab Shewangizaw",
+  profilePic: "./images/pp1.jpg",
+  likes: 100,
+  retweets: 200,
+  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+  replies: [
+    {
+      handle: "John Doe",
+      profilePic: "./images/pp2.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+    },
+    {
+      handle: "Jane Doe",
+      profilePic: "./images/pp3.jpg",
+      likes: 10,
+      retweets: 20,
+      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
+    }]
+}];
