4eacc6046c0a733f62fc4c8b5983183060faad97
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-17 00:47:24 +0300

    started the feed

--- index.js
+++ index.js
@@ -3,5 +3,27 @@ import { tweetsData } from './data.js';
 
 
 
+function feedCreator() {
+  let feedHTML = '';
+  tweetsData.forEach(tweet => {
+    feedHTML += `
+    <div class="tweet">
+      <div class="tweet-header">
+        <img src="${tweet.profilePic}" alt="Profile Picture" class="profile-pic">
+        <div class="handle">${tweet.handle}</div>
+      </div>
+      <div class="tweet-body">
+        <p>${tweet.tweetText}</p>
+          </div>
+        </div>`;
+      });
+  return feedHTML;
+    }
+    
 
-const feedHTML = ''
+
+function renderFeed() {
+  document.getElementById('feed').innerHTML = feedCreator();
+}
+
+renderFeed();
