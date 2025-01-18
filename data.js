24c4f56f00183d3d319c4324eb77bc7a7927d8a9
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-18 11:39:29 +0300

    some error here gona rewrite the js

--- data.js
+++ data.js
@@ -1,88 +1,98 @@
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+// import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-export const tweetsData = [{
-  handle: "Kaleab Shewangizaw",
-  profilePic: "./images/pp1.jpg",
-  id: uuidv4(),
-  likes: 100,
-  retweets: 200,
-  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
-  replies: [
-    {
-      handle: "Elon Musk",
-      profilePic: "./images/pp2.jpg",
-      id: uuidv4(),
-      likes: 10,
-      retweets: 20,
-      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
-    },
-    {
-      handle: "Github",
-      profilePic: "./images/pp3.jpg",
-      id: uuidv4(),
-      likes: 10,
-      retweets: 20,
-      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
-    }
-]},
-{
-  handle: "Elon Musk",
-  profilePic: "./images/pp2.jpg",
-  id: uuidv4(),
-  likes: 100,
-  retweets: 200,
-  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
-  replies: [
-    {
-      handle: "Kaleab Shewangizaw",
-      profilePic: "./images/pp1.jpg",
-      id: uuidv4(),
-      likes: 10,
-      retweets: 20,
-      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
-    },
-    {
-      handle: "Github",
-      profilePic: "./images/pp3.jpg",
-      id: uuidv4(),
-      likes: 10,
-      retweets: 20,
-      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
-    }
-]},
-{
-  handle: "Github",
-  profilePic: "./images/pp3.jpg",
-  id: uuidv4(),
-  likes: 100,
-  retweets: 200,
-  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
-  replies: [
-    {
-      handle: "Kaleab Shewangizaw",
-      profilePic: "./images/pp1.jpg",
-      id: uuidv4(),
-      likes: 10,
-      retweets: 20,
-      tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
-    },
-    {
-      handle: "John Doe",
-      profilePic: "./images/pp2.jpg",
-      id: uuidv4(),
-      likes: 10,
-      retweets: 20,
-      tweetText: "I am also replying to your tweet. #coding #javascript #reactjs",
-    }
-]},
-{
-  handle: "Someone",
-  profilePic: "./images/pp1.jpg",
-  id: uuidv4(),
-  likes: 100,
-  retweets: 200,
-  tweetText: "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
-  replies: []
-}];
-
-
+export const tweetsData = [
+  {
+    handle: "Kaleab Shewangizaw",
+    profilePic: "./images/pp1.jpg",
+    // id: uuidv4(),
+    likes: 100,
+    retweets: 200,
+    tweetText:
+      "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+    replies: [
+      {
+        handle: "Elon Musk",
+        profilePic: "./images/pp2.jpg",
+        // id: uuidv4(),
+        likes: 10,
+        retweets: 20,
+        tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+      },
+      {
+        handle: "Github",
+        profilePic: "./images/pp4.jpg",
+        // id: uuidv4(),
+        likes: 10,
+        retweets: 20,
+        tweetText:
+          "I am also replying to your tweet. #coding #javascript #reactjs",
+      },
+    ],
+  },
+  {
+    handle: "Elon Musk",
+    profilePic: "./images/pp2.jpg",
+    // id: uuidv4(),
+    likes: 100,
+    retweets: 200,
+    tweetText:
+      "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+    replies: [
+      {
+        handle: "Kaleab Shewangizaw",
+        profilePic: "./images/pp1.jpg",
+        // id: uuidv4(),
+        likes: 10,
+        retweets: 20,
+        tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+      },
+      {
+        handle: "Github",
+        profilePic: "./images/pp3.jpg",
+        // id: uuidv4(),
+        likes: 10,
+        retweets: 20,
+        tweetText:
+          "I am also replying to your tweet. #coding #javascript #reactjs",
+      },
+    ],
+  },
+  {
+    handle: "Github",
+    profilePic: "./images/pp3.jpg",
+    // id: uuidv4(),
+    likes: 100,
+    retweets: 200,
+    tweetText:
+      "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+    replies: [
+      {
+        handle: "Kaleab Shewangizaw",
+        profilePic: "./images/pp1.jpg",
+        // id: uuidv4(),
+        likes: 10,
+        retweets: 20,
+        tweetText: "I am replying to your tweet. #coding #javascript #reactjs",
+      },
+      {
+        handle: "John Doe",
+        profilePic: "./images/pp2.jpg",
+        // id: uuidv4(),
+        likes: 10,
+        retweets: 20,
+        tweetText:
+          "I am also replying to your tweet. #coding #javascript #reactjs",
+      },
+    ],
+  },
+  {
+    handle: "Someone",
+    profilePic: "./images/pp1.jpg",
+    // id: uuidv4(),
+    likes: 100,
+    retweets: 200,
+    tweetText:
+      "Here I am tweeting about something that I don't know about. #coding #javascript #reactjs",
+    replies: [],
+  },
+];
--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import { tweetsData } from './data.js';
-
-
-
+import { tweetsData } from "./data.js";
 
 function feedCreator() {
-  let feedHTML = '';
-  tweetsData.forEach(tweet => {
+  let feedHTML = "";
+  tweetsData.forEach((tweet) => {
     feedHTML += `
     <div class="tweet">
       <div class="tweet-header">
-        <img src="${tweet.profilePic}" alt="Profile Picture" class="profile-pic">
-        <div class="handle">${tweet.handle}</div>
+        <img src="${tweet.profilePic}" alt="avatar" />
+        <h2>${tweet.handle}</h2>  <!-- Fixed missing '<' -->
       </div>
-      <div class="tweet-body">
-        <p>${tweet.tweetText}</p>
-          </div>
-        </div>`;
-      });
+      <p>${tweet.tweetText}</p>
+      <div class="tweet-footer">
+        <span>${tweet.likes} Likes</span>
+        <span>${tweet.retweets} Retweets</span>
+        <span>${tweet.replies.length} Replies</span>  <!-- Fixed missing '<' -->
+      </div>
+    </div>
+    `;
+  });
   return feedHTML;
-    }
-    
-
-
-function renderFeed() {
-  document.getElementById('feed').innerHTML = feedCreator();
 }
 
-renderFeed();
+function renderFeed() {
+  const theFeed = document.getElementById("feed");  // Correctly obtain the element
+  theFeed.innerHTML = feedCreator();  // Set innerHTML to the generated feed
+  console.log(feedCreator());
+}
+
+// Wait for the DOM to load before running renderFeed
+document.addEventListener("DOMContentLoaded", renderFeed);
